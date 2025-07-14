ad4a663db1cf342110650824a72b2f5e80cea152
Kacper Lechicki <160114199+Kacper-Lechicki@users.noreply.github.com>
2025-07-15 00:05:33 +0200

    Feat/landing page (#3)
    * config: setup tailwind / daisy ui / iconify
    
    * feat: add landing page

--- app.vue
+++ app.vue
@@ -1,11 +1,6 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppNavbar />
+
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>

c4132f0975611ef8b82d2bf4a30808898c14b781
Kacper Lechicki <klechicki.wrk@gmail.com>
2025-07-15 00:02:53 +0200

    feat: add landing page

---
+++ components/app/navbar.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink class="btn btn-ghost text-xl" to="/">
+        Travel Log
+      </NuxtLink>
+    </div>
+
+    <div class="navbar-end">
+      <a class="btn btn-accent">
+        Sign In With Github
+        <Icon name="tabler:brand-github" size="24" /></a>
+    </div>
+  </div>
+</template>

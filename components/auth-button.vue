2848ba64f37ad97abc62fe794e115d9a0565fd6f
Kacper Lechicki <160114199+Kacper-Lechicki@users.noreply.github.com>
2025-07-22 01:01:55 +0200

    feat: login / logout (auth session) (#11)

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -3,13 +3,38 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div
+      class="btn mt-1 flex gap-3"
+      role="button"
+      tabindex="0"
+    >
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-6 rounded-full">
+          <img :src="authStore.user.image" alt="User avatar">
+        </div>
+      </div>
+
+      <span>{{ authStore.user.name }}</span>
+    </div>
+
+    <ul class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-md" tabindex="0">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="18" />
+          <span>Sign out</span>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+
   <button
+    v-else
     :disabled="authStore.loading"
     class="btn btn-accent"
     @click="authStore.signIn"
   >
     <span>Sign In With Github</span>
-
     <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
 
     <Icon
